05cdfc5c5af0a05436e56ae6fafb5ef17386f402
Valerie Newton <newton_lav@yahoo.com>
2025-01-16 15:53:25 -0500

    added index.js file, added media queries

---
+++ js/index.js
@@ -0,0 +1,21 @@
+//Create footer and add it to the body
+const body = document.querySelector("body");
+const footer = document.createElement("footer");
+body.appendChild(footer);
+
+// //Insert copyright text into footer
+// let today = new Date();
+
+
+// //create list of Skills
+// const skills = ["JavaScript", "HTML", "CSS", "Geographic Information Systems", "ArcPro", "ArcGIS Online", "SalesForce", "Tessitura", "Spanish Language Proficiency"];
+
+// const skillsSection = document.querySelector("#skills");
+
+// const skillsList = skillsSection.querySelector("ul");
+
+// for (let i = 0; i < skills.length; i++) {
+//     skill = skillsList.createElement("li");
+//     skill = skills[i].textContent;
+//     skillsList.appendChild(skill);
+// }
