0183adc1a58afd877d9d8fbad78e88b1f39be6c3
Valerie Newton <newton_lav@yahoo.com>
2025-01-16 21:17:10 -0500

    used js to add skills list and footer elements. Styled skills section.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,37 @@
+
+//create alert for survey button
+const surveyBtn = document.querySelector(".survey-btn");
+function alertThanks() {
+    alert("Thanks for your submission!")
+}
+surveyBtn.addEventListener("click", alertThanks)
+
+
+//create list of Skills
+const skills = ["JavaScript", "HTML", "CSS", "Geographic Information Systems", "ArcPro", "ArcGIS Online", "ArcGIS Field Maps", "Plant Predict", "Anderson Optimization", "SalesForce", "Tessitura", "Epic", "Renewable Energy", "Spanish Language Proficiency"];
+
+const skillsSection = document.querySelector("#skills");
+
+const skillsList = skillsSection.querySelector("ul");
+// skillsList.classList.add("skillsBullets");
+
+for (let i = 0; i < skills.length; i++) {
+    let skill = document.createElement("li");
+    skill.textContent = skills[i];
+    skillsList.appendChild(skill);
+}
+
 //Create footer and add it to the body
 const body = document.querySelector("body");
 const footer = document.createElement("footer");
 body.appendChild(footer);
 
+
 // //Insert copyright text into footer
-// let today = new Date();
+let today = new Date();
+const thisYear = today.getFullYear();
 
+const copyright = document.createElement("p");
+copyright.textContent = "\u00A9 Valerie Newton " + thisYear;
 
-// //create list of Skills
-// const skills = ["JavaScript", "HTML", "CSS", "Geographic Information Systems", "ArcPro", "ArcGIS Online", "SalesForce", "Tessitura", "Spanish Language Proficiency"];
-
-// const skillsSection = document.querySelector("#skills");
-
-// const skillsList = skillsSection.querySelector("ul");
-
-// for (let i = 0; i < skills.length; i++) {
-//     skill = skillsList.createElement("li");
-//     skill = skills[i].textContent;
-//     skillsList.appendChild(skill);
-// }
+footer.appendChild(copyright);
