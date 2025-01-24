bebd51548c652a1d2de41fe622434e3434fce54d
Valerie Newton <newton_lav@yahoo.com>
2025-01-24 14:45:15 -0500

    changed logos in connect section, edited if/else statement for hidden message board

--- js/index.js
+++ js/index.js
@@ -45,6 +45,7 @@ messageForm.addEventListener("submit", handleSubmit);
 let messageBoard = document.querySelector("#messageboard");
 let messagesList = messageBoard.querySelector("ul");
 let numMessages = messagesList.children.length;
+console.log("messages before submit: " + numMessages);
 
 if(numMessages === 0) {
     messageBoard.hidden = true;
@@ -65,8 +66,10 @@ function handleSubmit(event) {
     let messageSection = document.querySelector("#messageboard");
     let messageList = messageSection.querySelector("ul");
     let newMessage = document.createElement("li");
+    let newMessageCount = messageList.childElementCount;
     newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a> <br> <span>${usersMessage}</span>`;
 
+    
     //create remove & edit buttons
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
@@ -92,12 +95,15 @@ function handleSubmit(event) {
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
 
-    //unhide message board
-    messageBoard.hidden = false;
-    
-
     //clear form after use
     messageForm.reset();
+
+    console.log("messages after submit: " + newMessageCount);
+
+    //re-hide messageboard if no messages
+    if(numMessages === 0) {
+        messageBoard.hidden = true;
+        } else messageBoard.hidden = false;
 }
 
 
