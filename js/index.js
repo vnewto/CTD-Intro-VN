1d2a358ce69ef70d4fa25cd2a1c0d72cebce2d7f
Valerie Newton <newton_lav@yahoo.com>
2025-01-27 17:43:42 -0500

    added projects list and styled projects section

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-//create alert for survey button
-const surveyBtn = document.querySelector(".survey-btn");
-function alertThanks() {
-    alert("Thanks for your submission!");
-}
-surveyBtn.addEventListener("click", alertThanks);
-
-
 //create list of Skills
 const skills = ["JavaScript", "HTML", "CSS", "Geographic Information Systems", "ArcPro", "ArcGIS Online", "ArcGIS Field Maps", "Plant Predict", "Anderson Optimization", "SalesForce", "Tessitura", "Epic", "Renewable Energy", "Spanish Language Proficiency"];
 
@@ -43,13 +35,7 @@ messageForm.addEventListener("submit", handleSubmit);
 
 //hide message section if no messages
 let messageBoard = document.querySelector("#messageboard");
-let messagesList = messageBoard.querySelector("ul");
-let numMessages = messagesList.children.length;
-console.log("messages before submit: " + numMessages);
-
-if(numMessages === 0) {
-    messageBoard.hidden = true;
-    };
+messageBoard.hidden = true;
 
 
 function handleSubmit(event) {
@@ -60,13 +46,11 @@ function handleSubmit(event) {
     let usersName = event.target.usersName.value;
     let usersEmail = event.target.usersEmail.value;
     let usersMessage = event.target.usersMessage.value;
-    console.log(usersName, usersEmail, usersMessage);
 
     //create variables to display messages
     let messageSection = document.querySelector("#messageboard");
     let messageList = messageSection.querySelector("ul");
     let newMessage = document.createElement("li");
-    let newMessageCount = messageList.childElementCount;
     newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a> <br> <span>${usersMessage}</span>`;
 
     
@@ -78,6 +62,11 @@ function handleSubmit(event) {
     function handleRemove(event) {
         let entry = event.currentTarget.parentNode;
         entry.remove();
+        //re-hide messageboard if no messages
+        let newMessageCount = messageList.childElementCount;
+        if(newMessageCount === 0) {
+            messageBoard.hidden = true;
+            } else messageBoard.hidden = false;
     }
 
     const editButton = document.createElement("button");
@@ -98,12 +87,80 @@ function handleSubmit(event) {
     //clear form after use
     messageForm.reset();
 
-    console.log("messages after submit: " + newMessageCount);
+    messageBoard.hidden = false;
 
-    //re-hide messageboard if no messages
-    if(numMessages === 0) {
-        messageBoard.hidden = true;
-        } else messageBoard.hidden = false;
 }
+
+
+//Handle submit button for Energy Survey
+const energySurvey = document.forms["energy_survey"];
+energySurvey.addEventListener("submit", handleEnergySurvey);
+
+function handleEnergySurvey(event) {
+    //prevent page from refreshing upon form submission
+    event.preventDefault();
+
+    //create variables to collect name of energy interests
+    let energyInterests = {
+        solar: "https://education.nationalgeographic.org/resource/solar-energy/", 
+        wind: "https://education.nationalgeographic.org/resource/wind-energy/", 
+        hydro: "https://education.nationalgeographic.org/resource/hydroelectric-energy/", 
+        geothermal: "https://education.nationalgeographic.org/resource/geothermal-energy/",
+        ocean: "https://education.nationalgeographic.org/resource/tidal-energy/", 
+        hydrogen: "https://www.nationalgeographic.com/environment/article/hydrogen-from-sunlight", 
+        biomass: "https://education.nationalgeographic.org/resource/biomass-energy/"
+    };
+    let energySelection = event.target.energy_interest.value;
+    let energySelectionStrong = energySelection.toUpperCase();
+
+    //create new div to put in energy-survey section
+    let sidebar = document.querySelector(".sidebar");
+    let surveyResponseContainer = document.createElement("div");
+
+    //create variables for response
+    let energyWebsite = energyInterests[energySelection];
+    let surveyResponse = document.createElement("p");
+    surveyResponse.classList.add(["p-sidebar"]);
+    surveyResponse.classList.add(["survey-container"]);
+    surveyResponse.innerHTML = `Thank you for your interest in <strong>${energySelectionStrong}</strong> energy! Check out this <a href="${energyWebsite}">article from National Geographic</a> to learn more.`
+
+    //add new section and text into HTML
+    sidebar.appendChild(surveyResponseContainer);
+    surveyResponseContainer.appendChild(surveyResponse);
+
+    //clear form after use
+    energySurvey.reset();
+
+}
+
+//GET request to get project data from github
+const projectSection = document.querySelector("#projects");
+const projectList = projectSection.querySelector("ul");
+
+fetch("https://api.github.com/users/vnewto/repos")
+    .then((response) => {
+        //show error if needed
+        if(!response.ok) {
+            throw new Error("Data not found");
+        }
+        return response.json();
+    })
+    .then((repositories) => {
+        //put project data into Project section
+        for(let i = 0; i < repositories.length; i++) {
+            let project = document.createElement("li")
+            project.innerHTML = `<a href="${repositories[i].html_url}">${repositories[i].name}</a>`;
+            projectList.appendChild(project);
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+        let errorMessage = document.createElement("p");
+        errorMessage.innerHTML = error;
+        projectList.appendChild(errorMessage);
+    });
+
+
+
 
 
